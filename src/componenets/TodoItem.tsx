9c329269aed796af026543425478153f5bf182c8
iSalah-YT <kurditstation@gmail.com>
2023-07-17 10:12:23 +0100

    project completed

---
+++ src/componenets/TodoItem.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+type TodoItemProp = {
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+};
+
+export function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+}: TodoItemProp) {
+  return (
+    <li className="flex gap-1 items-center">
+      <input
+        id={id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
+      <label
+        htmlFor={id}
+        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
+    </li>
+  );
+}
